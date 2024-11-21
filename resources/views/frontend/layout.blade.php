0b00c872617aa5d23a201a61ab11dd861ed8012d
Andre Firman <syarqian24@gmail.com>
2024-11-21 10:24:26 +0800

    Update

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="icon" href="img/favicon.png" type="image/png" />
-    <title>Eiser ecommerce</title>
+    <title>Oleh Etam</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('user_assets/css/bootstrap.css') }}" />
     <link rel="stylesheet" href="{{ asset('user_assets/vendors/linericon/style.css') }}" />
